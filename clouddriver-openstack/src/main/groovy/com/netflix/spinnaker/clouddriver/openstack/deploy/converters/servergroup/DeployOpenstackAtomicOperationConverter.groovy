b29e64c529adaf685b4e4a3d394f35d9b6b8b0c3
drmaas <daniel.maas@target.com>
2016-06-14 11:56:25 -0500

    fixing header

--- clouddriver-openstack/src/main/groovy/com/netflix/spinnaker/clouddriver/openstack/deploy/converters/servergroup/DeployOpenstackAtomicOperationConverter.groovy
+++ clouddriver-openstack/src/main/groovy/com/netflix/spinnaker/clouddriver/openstack/deploy/converters/servergroup/DeployOpenstackAtomicOperationConverter.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Target, Inc.
+ * Copyright 2016 Veritas Technologies LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
